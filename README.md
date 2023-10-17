df8043069941278520c8f53d3571bf4409f98026
AlexCr_204 <alexander09cruz@hotmail.com>
2023-10-16 21:45:06 -0600

    Make a Route and Link to it

--- README.md
+++ README.md
@@ -11,9 +11,46 @@ Modificamos el welcom.blade.php que cargaba la vista basica de laravel creando n
 ![Alt text](image-2.png)
 
 Agregamos un archivo app.css para dar estilo por medio de css
+
 ![Alt text](image-3.png)
 
 Agregamos un archivo app.js para crear codigo javascript
+
 ![Alt text](image-4.png)
 
+## Make a Route and Link to it
+Modificamos el nombre de la vista en la carpeta views y el llamado en el archivo web.php de la carpeta routes
 
+![Alt text](image-6.png)
+
+Cambiamos el contenido del html de posts.blade.php
+
+![Alt text](image-5.png)
+
+Junto con el css
+
+![Alt text](image-7.png)
+
+Hacemos los titulos de cada post cliqueables y con un link que redireccione a otra pagina
+
+![Alt text](image-8.png)
+
+![Alt text](image-9.png)
+
+![Alt text](image-10.png)
+
+Esto se logra creando una nueva vista en la carpeta views
+
+![Alt text](image-11.png)
+
+Y una nueva ruta en web.php
+
+![Alt text](image-12.png)
+
+Una vez creada la nueva vista del post individual, creamos un boton para regresar al Home
+
+![Alt text](image-14.png)
+
+![Alt text](image-13.png)
+
+## 
---
+++ webserver/sites/lfts.isw811.xyz/resources/views/posts.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+</head>
+
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam doloremque sint a, molestiae nobis doloribus temporibus, corrupti veniam dignissimos id at! Beatae officiis debitis a facilis accusamus modi sit eum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga expedita nihil quia soluta facere consequatur blanditiis incidunt neque sit pariatur iure rerum nulla, suscipit amet, ducimus harum, facilis delectus nesciunt!
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam doloremque sint a, molestiae nobis doloribus temporibus, corrupti veniam dignissimos id at! Beatae officiis debitis a facilis accusamus modi sit eum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga expedita nihil quia soluta facere consequatur blanditiis incidunt neque sit pariatur iure rerum nulla, suscipit amet, ducimus harum, facilis delectus nesciunt!
+        </p>
+    </article>
+    
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam doloremque sint a, molestiae nobis doloribus temporibus, corrupti veniam dignissimos id at! Beatae officiis debitis a facilis accusamus modi sit eum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga expedita nihil quia soluta facere consequatur blanditiis incidunt neque sit pariatur iure rerum nulla, suscipit amet, ducimus harum, facilis delectus nesciunt!
+        </p>
+    </article>
+</body>
+</html>
