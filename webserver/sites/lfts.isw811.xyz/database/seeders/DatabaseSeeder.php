e19cdfa043fcc10a7fc12ba71cbb27f2be67aa3a
AlexCr_204 <alexander09cruz@hotmail.com>
2023-10-19 21:50:26 -0600

    Turbo Boost With Factories

--- webserver/sites/lfts.isw811.xyz/database/seeders/DatabaseSeeder.php
+++ webserver/sites/lfts.isw811.xyz/database/seeders/DatabaseSeeder.php
@@ -16,44 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();  
-        Post::truncate();
-
-        $user = User::Factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
+        $user = User::factory()->create([
+            'name' => 'Alex Cruz',
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        Post::create([
+        Post::factory(5)->create([
             'user_id'=> $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita accusamus quaerat aliquid incidunt nihil iste sed velit ipsam, suscipit molestias dicta fugiat odio officia omnis reprehenderit minus quidem minima.odio officia</p>',
         ]);
-
-        Post::create([
-            'user_id'=> $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita accusamus quaerat aliquid incidunt nihil iste sed velit ipsam, suscipit molestias dicta fugiat odio officia omnis reprehenderit minus quidem minima.odio officia</p>',
-        ]);
-
     }
 }
