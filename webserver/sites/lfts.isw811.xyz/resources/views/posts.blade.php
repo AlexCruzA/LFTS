265348dc8f521fbfa19deeb9c51e849fb64425d8
AlexCr_204 <alexander09cruz@hotmail.com>
2023-10-19 19:40:42 -0600

    Your First Eloquent Relationship

--- webserver/sites/lfts.isw811.xyz/resources/views/posts.blade.php
+++ webserver/sites/lfts.isw811.xyz/resources/views/posts.blade.php
@@ -8,6 +8,11 @@
                     {{ $post->title }}
                 </a>
             </h1>
+
+            <p>
+                <a href="#">{{ $post->category->name }}</a>
+            </p>
+
             <div>
                 {{ $post->excerpt }}
             </div>
