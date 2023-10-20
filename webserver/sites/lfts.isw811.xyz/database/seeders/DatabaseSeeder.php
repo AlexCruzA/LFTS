d98a5a7a85901d91fa2fb68119d8b074972a4814
AlexCr_204 <alexander09cruz@hotmail.com>
2023-10-19 21:19:56 -0600

    Database Seeding Saves Time

--- webserver/sites/lfts.isw811.xyz/database/seeders/DatabaseSeeder.php
+++ webserver/sites/lfts.isw811.xyz/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();  
+        Post::truncate();
+
+        $user = User::Factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family',
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+
+        Post::create([
+            'user_id'=> $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>',
+            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita accusamus quaerat aliquid incidunt nihil iste sed velit ipsam, suscipit molestias dicta fugiat odio officia omnis reprehenderit minus quidem minima.odio officia</p>',
+        ]);
+
+        Post::create([
+            'user_id'=> $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>',
+            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita accusamus quaerat aliquid incidunt nihil iste sed velit ipsam, suscipit molestias dicta fugiat odio officia omnis reprehenderit minus quidem minima.odio officia</p>',
+        ]);
+
     }
 }
--- webserver/sites/lfts.isw811.xyz/resources/views/post.blade.php
+++ webserver/sites/lfts.isw811.xyz/resources/views/post.blade.php
@@ -5,7 +5,7 @@
     <article>
         <h1>{{ $post->title }}</h1>
         <p>
-            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            By <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
         </p>
         <div>
             {!! $post->body !!}
