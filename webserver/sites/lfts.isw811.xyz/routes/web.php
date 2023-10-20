fc24effef820ca18075c3c552870c4e20527cb98
AlexCr_204 <alexander09cruz@hotmail.com>
2023-10-19 19:56:59 -0600

    Show All Posts Associated With a Category

--- webserver/sites/lfts.isw811.xyz/routes/web.php
+++ webserver/sites/lfts.isw811.xyz/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 
@@ -14,3 +15,8 @@ Route::get('posts/{post:slug}', function (Post $post) {
     ]);
 });
 
+Route::get('categories/{category:slug}', function (Category $category) {    
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
+});
